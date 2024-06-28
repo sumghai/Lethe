d3f806f3674d2969f7edd67ae34096960d50f340
sumghai <sumghai@users.noreply.github.com>
2024-06-28 21:41:06 +1200

    Spawn Iso Datachip artifact 1 day after vanilla research condition met

---
+++ Source/GameComponent/GameComponent_Lethe.cs
@@ -0,0 +1,53 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace Lethe
+{
+    public class GameComponent_Lethe : GameComponent
+    {
+        public const int spawnIsoDataChipDelayDays = 1;
+
+        public const int spawnIsoDataChipDelayTicks = spawnIsoDataChipDelayDays * GenDate.TicksPerDay;
+        
+        public int spawnIsoDataChipTick = -99999;
+
+        public bool shouldSpawnIsoDataChip = true;
+
+        public GameComponent_Lethe(Game game)
+        { 
+        }
+
+        public override void GameComponentTick()
+        {
+            if (shouldSpawnIsoDataChip && Find.TickManager.TicksGame % GenTicks.TickLongInterval == 0 && LetheDefOf.AdvancedFabrication.IsFinished) 
+            {
+                spawnIsoDataChipTick = Find.TickManager.TicksGame + spawnIsoDataChipDelayTicks;
+                shouldSpawnIsoDataChip = false;
+            }
+
+            if (spawnIsoDataChipTick > 0 && Find.TickManager.TicksGame > spawnIsoDataChipTick) 
+            {
+                SpawnIsoDataChipNearResearchBench();
+                spawnIsoDataChipTick = -99999;
+            }
+        }
+
+        public void SpawnIsoDataChipNearResearchBench()
+        {
+            Map spawnMap = Find.Maps.Where(m => m.IsPlayerHome).FirstOrDefault();
+            IntVec3 spawnPos = spawnMap.listerBuildings.AllBuildingsColonistOfClass<Building_ResearchBench>().First().Position;
+            Thing isoDataChip = ThingMaker.MakeThing(LetheDefOf.Lethe_Artifact_IsolinearDatachip);
+            GenPlace.TryPlaceThing(isoDataChip, spawnPos, spawnMap, ThingPlaceMode.Near);
+            Find.LetterStack.ReceiveLetter("Lethe_Letter_IsoDataChip".Translate(), "Lethe_Letter_IsoDataChipDesc".Translate("Jury-Rigged Datachip Reader".Colorize(ColoredText.NameColor), LetheDefOf.Lethe_Cipher1.LabelCap.Colorize(ColoredText.NameColor)), LetterDefOf.NeutralEvent, isoDataChip);
+            // todo - replace hardcoded name for Jury-Rigged Datachip Reader with label of actual building
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref shouldSpawnIsoDataChip, "shouldSpawnIsoDataChip", defaultValue: false);
+            Scribe_Values.Look(ref spawnIsoDataChipTick, "spawnIsoDataChipTick", 0, true);
+        }
+    }
+}
---
+++ Source/LetheDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Lethe
+{
+    [DefOf]
+    public static class LetheDefOf
+    {
+        public static ResearchProjectDef AdvancedFabrication;
+        public static ResearchProjectDef Lethe_Cipher1;
+
+        public static ThingDef Lethe_Artifact_IsolinearDatachip;
+    }
+}
